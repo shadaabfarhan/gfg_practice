952d1fbe98a36c37e6a88428818c1934d22fae55
MD Shadaab Farhan <60579108+shadaabfarhan@users.noreply.github.com>
2020-07-13 23:26:28 +0530

    Create binary_search.cpp

---
+++ binary_search.cpp
@@ -0,0 +1,19 @@
+/*You need to complete this function */
+int bin_search(int A[], int left, int right, int k)
+{
+    if(left>right){
+        return -1;
+    }
+    
+    int mid = (left + right)/2;
+    
+    if(A[mid] == k){
+        return mid;
+    }
+    else if(A[mid]>k){
+        return bin_search(A,left,mid-1,k);
+    }
+    else{
+        return bin_search(A,mid+1,right,k);
+    }
+}
